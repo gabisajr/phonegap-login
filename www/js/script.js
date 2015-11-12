1f0b4f51172bc9a5ddd0459f2f105a6649d3e19a
Manuel Jesus Garcia Ruiz <mjgr0013@gmail.com>
2015-11-12 01:23:40 +0100

    Add funcion de buscar el usuario al clicar en entrar

--- www/js/script.js
+++ www/js/script.js
@@ -6,6 +6,7 @@ $(function() {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
+
 	$('#register-form-link').click(function(e) {
 		$("#register-form").delay(100).fadeIn(100);
  		$("#login-form").fadeOut(100);
@@ -14,4 +15,57 @@ $(function() {
 		e.preventDefault();
 	});
 
+	$("#login-submit").click(function(e) {
+		iniciarSesion();
+	});
+
+	$('#login-form').keypress(function (e) {
+ 		var key = e.which;
+		if(key == 13){  // the enter key code
+		  $("#login-submit").click();
+		}
 });
+
+});
+
+function iniciarSesion () {
+	var nombreUsuario = $("#username").val();
+	var pass = $("#password").val();
+
+  $.ajax({
+    data:  {
+    	"sentencia" : '0',
+    	"nombreUsuario" : nombreUsuario,
+    	"pass" : pass
+  	},
+    url: "php/server.php",
+    type:  'post',
+    beforeSend: function () {
+            console.log("Iniciando peticion ...")
+    },
+    success:  function (response) {
+			if(response) {
+				console.log("Usuario encontrado");
+			}else {
+				console.log("Usuario no encontrado");
+			}
+    }
+  });
+}
+
+function registrarUsuario () {
+  $.ajax({
+    data:  {
+    	"valorCaja1" : 'a',
+    	"valorCaja2" : 'b'
+  	},
+    url: "php/server.php",
+    type:  'post',
+    beforeSend: function () {
+            $("#resultado").html("Procesando, espere por favor...");
+    },
+    success:  function (response) {
+            console.log(response);
+    }
+  });
+}
---
+++ www/php/server.php
@@ -0,0 +1,82 @@
+<?php 
+	header('content-type: application/json; charset=utf-8');
+	// Para que se puedan conectar los terminales //
+	header("access-control-allow-origin: *");
+
+	date_default_timezone_set('Europe/Madrid');
+
+	try {
+		$dbh = new PDO('mysql:host=localhost;dbname=login;charset=utf8','root', '');
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+		die('Ha ocurrido un error al conectar con la base de datos');
+	}
+
+	// Función para buscar si existe el usuario //
+	function buscarUsuario ($dbh, $nombreUsuario, $pass) {
+		$consulta = $dbh->prepare('SELECT * FROM usuario WHERE nombreUsuario=:nombreUsuario AND pass=:pass');
+		$consulta->bindParam(':nombreUsuario', $nombreUsuario , PDO::PARAM_STR);
+		$consulta->bindParam(':pass', $pass, PDO::PARAM_STR);
+		$consulta->execute();
+		return $consulta->fetch(PDO::FETCH_ASSOC);
+	}
+
+	// Función para buscar si existe la sesion //
+	function existeSesion($dbh, $token) {
+		$consulta = $dbh->prepare('SELECT * FROM sesion WHERE token=:token');
+		$consulta->bindParam(':token', $token , PDO::PARAM_STR);
+		$consulta->execute();
+		return $consulta->fetch(PDO::FETCH_ASSOC);
+	}
+
+	// Función para registrar un usuario //
+	function registrarUsuario ($dbh, $nombreUsuario, $pass) {
+		$consulta = $dbh->prepare('INSERT INTO usuario (nombreUsuario, pass) VALUES (:nombreUsuario, :pass)');
+		$consulta->bindParam(':nombreUsuario', $nombreUsuario , PDO::PARAM_STR);
+		$consulta->bindParam(':pass', $pass , PDO::PARAM_STR);
+		$consulta->execute();
+		return $consulta->rowCount();
+	}
+
+		// Función para registrar una sesion //
+	function registrarSesion ($dbh, $idUser) {
+
+		$date = date('Y/m/d H:i:s');
+		$date = strtotime($date);
+		$date = strtotime("+10 day", $date);
+
+		$consulta = $dbh->prepare('INSERT INTO sesion (idUser, token, fechaInicio, fechaFin) VALUES (:idUser, :token, :fechaInicio, :fechaFin)');
+		$consulta->bindParam(':idUser', $idUser , PDO::PARAM_INT);
+		$consulta->bindParam(':fechaInicio', date('Y/m/d H:i:s'), PDO::PARAM_STR);
+		$consulta->bindParam(':fechaFin', date('Y/m/d H:i:s', $date) , PDO::PARAM_STR);
+		$consulta->bindParam(':token', generateRandomString(15), PDO::PARAM_STR);
+		$consulta->execute();
+		return $consulta->rowCount();
+	}
+
+	// Función para generar una cadena aleatoria de longitud variable //
+	function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+	}
+
+	switch ($_POST['sentencia']) {
+    case 0:
+        $resultado = buscarUsuario($dbh,$_POST['nombreUsuario'],$_POST['pass']);
+        echo json_encode($resultado);
+        break;
+    case 1:
+        echo "i es igual a 1";
+        break;
+    case 2:
+        echo "i es igual a 2";
+        break;
+}
+
+?>
